d96ff3871ab2ee69f32432272d61590d17af30c3
Antonio <antoniomorara@gmail.com>
2019-04-07 00:40:29 -0300

    Point Recording Added. To see => log

--- canvas.js
+++ canvas.js
@@ -5,10 +5,47 @@ var context = canvas.getContext("2d");
 var points = [];
 // How thick
 let line_width = 2;
+let mode = "None";
+
+// Registers left mouse clicks
+document.getElementById('canvas').addEventListener('click', mouseLeftClick, false);
 
 
 // Clears canvas board
-function clear(){
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+function clearBoard(){
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  mode = "None";
   points = [];
+}
+
+// All the pollowing functions follow the same principle
+// Change the draw mode
+function modeBezier(){
+	mode = "Bezier";
+	points = [];
+}
+
+function modeLine(){
+	mode = "Line";
+	points = [];
+}
+
+function modeCircle(){
+	mode = "Circle";
+	points = [];
+}
+
+function modePolygon(){
+	mode = "Polygon";
+	points = [];
+}
+
+function mouseLeftClick(event){
+	// If there is a draw mode, stack the points
+	if(mode != "None"){
+		var x = event.clientX;
+		var y = event.clientY;
+		points.push([x,y]);
+		console.log("X:"+x+", Y:"+y+", Mode:"+mode);
+	}
 }
